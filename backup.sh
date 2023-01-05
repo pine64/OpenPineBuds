cc1738ef0bdf5e6a9cfc58086a10618800400ec2
Thatcher Chamberlin <j.thatcher.c@gmail.com>
2023-01-04 20:55:59 -0500

    Added backup.sh script

---
+++ backup.sh
@@ -0,0 +1,21 @@
+#! /bin/bash
+
+BKPPATH="firmware-backups"
+
+mkdir -p $BKPPATH
+
+NOW=$(date +%s)
+
+num=$(ls -l /dev/ttyACM* | rev | cut -c 1)
+echo com is:$num
+splitPorts=($num)
+echo "This tool assumes your buds are the only thing connected and are enumerated {right,left} order. YMMV"
+echo "Right bud is at ${splitPorts[0]}"
+echo "Left bud is at ${splitPorts[1]}"
+
+echo "Please disconnect and reconnect the bud on the right"
+
+bestool read-image --port /dev/ttyACM${splitPorts[0]} $BKPPATH/firmware-$NOW-${splitPorts[0]}.bin.bkp
+
+echo "Please disconnect and reconnect the bud on the left"
+bestool read-image --port /dev/ttyACM${splitPorts[1]} $BKPPATH/firmware-$NOW-${splitPorts[1]}.bin.bkp
