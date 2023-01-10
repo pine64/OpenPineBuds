ef84de0b949e76e37b81db15da57f924777c30dd
Thatcher Chamberlin <j.thatcher.c@gmail.com>
2023-01-09 20:42:41 -0500

    Added output name to convert.sh

--- convert.sh
+++ convert.sh
@@ -1,21 +1,21 @@
 #!/bin/sh
 txt_to_wav() {
-  xxd -r -p $args > out.raw
-  ffmpeg -f sbc -ac 1 -i ./out.raw output.wav
+  xxd -r -p $arg1 > out.raw
+  ffmpeg -f sbc -ac 1 -i ./out.raw $arg2
   rm ./out.raw
 }
 
 wav_to_txt() {
-  ffmpeg -i $args -f sbc -ac 16000 -ac 1 -map_metadata -1 out.raw
+  ffmpeg -i $arg1 -f sbc -ac 16000 -ac 1 -map_metadata -1 out.raw
   xxd -i ./out.raw | head -n -2 \
     | tail -n +2 | sed 's/ //g' \
     | tr --delete '\n' \
-    | sed 's/,/\,\n/16; P; D' > SOUND.txt
+    | sed 's/,/\,\n/16; P; D' > $arg2
   rm ./out.raw
 }
 
-[ ${1} = "-T" ] || [ ${1} = "--txt-to-wav" ] && shift 1 && args=$@ && txt_to_wav && exit
-[ ${1} = "-W" ] || [ ${1} = "--wav-to-txt" ] && shift 1 && args=$@ &&  wav_to_txt && exit
+[ ${1} = "-T" ] || [ ${1} = "--txt-to-wav" ] && shift 1 && args=$@ && arg1=$(echo $args | cut -d" " -f1) && arg2=$(echo $args | cut -d" " -f2) && txt_to_wav && exit
+[ ${1} = "-W" ] || [ ${1} = "--wav-to-txt" ] && shift 1 && args=$@ && arg1=$(echo $args | cut -d" " -f1) && arg2=$(echo $args | cut -d" " -f2) &&  wav_to_txt && exit
 echo "
 Sound format converter:
 Usage:
