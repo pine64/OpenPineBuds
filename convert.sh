2a0ff6569dd81f74f9f30714de8309013b4f8d9a
Thatcher Chamberlin <j.thatcher.c@gmail.com>
2023-01-10 20:37:29 -0500

    Added hash to out.raw so that `make -j` doesn't fail

--- convert.sh
+++ convert.sh
@@ -1,17 +1,19 @@
 #!/bin/sh
 txt_to_wav() {
-  xxd -r -p $arg1 > out.raw
-  sbcinfo out.raw
+  hash=$(echo $arg1 | md5sum | cut -d" " -f1)
+  xxd -r -p $arg1 > out-$hash.raw
+  sbcinfo out-$hash.raw
   ffmpeg -y \
     -v info         `# verbosity - other options are "quiet", "error", "panic"` \
     -f sbc          `# accept SBC format` \
     -ac 1           `# audio channel: #1` \
-    -i ./out.raw    `# input file: out.raw` \
+    -i ./out-$hash.raw    `# input file: out.raw` \
     $arg2           `# output to $arg2`
-  rm out.raw
+  rm out-$hash.raw
 }
 
 wav_to_txt() {
+  hash=$(echo $arg1 | md5sum | cut -d" " -f1)
   ffmpeg -y \
     -v info            `# verbosity - other options are "quiet", "error", "panic"` \
     -i $arg1           `#input file: $arg1` \
@@ -20,17 +22,17 @@ wav_to_txt() {
     -ac 1              `# audio channel: #1` \
     -aq 16             `# audio quality: 16 (for SBC this means bitpool=16)` \
     -map_metadata -1   `# ????` \
-    out.raw            `# output to out.raw`
+    out-$hash.raw            `# output to out.raw`
 
-  sbcinfo out.raw
-  xxd -i out.raw            `# output in C include file style` \
+  sbcinfo out-$hash.raw
+  xxd -i out-$hash.raw            `# output in C include file style` \
     | head -n -2              `# skip last two xxd outline lines (skip C formatting)` \
     | tail -n +2              `# start output on line 2 of xxd output (skip C formatting)` \
     | sed 's/ //g'            `# remove spaces` \
     | tr --delete '\n'        `# remove newlines` \
     | sed 's/,/\,\n/16; P; D' `# collect into lines with the right length` \
     > $arg2
-  rm out.raw
+  rm out-$hash.raw
 }
 
 [ "${1}" = "-T" ] || [ "${1}" = "--txt-to-wav" ] && shift 1 && args=$@ && arg1=$(echo $args | cut -d" " -f1) && arg2=$(echo $args | cut -d" " -f2) && txt_to_wav && exit
