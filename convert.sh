e8d47b082a425074f0f16a29d0e54726aac1fa68
Thatcher Chamberlin <j.thatcher.c@gmail.com>
2023-01-12 18:26:18 -0500

    Changed out.raw to out.sbc and commented out the usage of the sbcinfo command

--- convert.sh
+++ convert.sh
@@ -1,15 +1,15 @@
 #!/bin/sh
 txt_to_wav() {
   hash=$(echo $arg1 | md5sum | cut -d" " -f1)
-  xxd -r -p $arg1 > out-$hash.raw
-  sbcinfo out-$hash.raw
+  xxd -r -p $arg1 > out-$hash.sbc
+  # sbcinfo out-$hash.sbc
   ffmpeg -y \
     -v info         `# verbosity - other options are "quiet", "error", "panic"` \
     -f sbc          `# accept SBC format` \
     -ac 1           `# audio channel: #1` \
-    -i ./out-$hash.raw    `# input file: out.raw` \
+    -i ./out-$hash.sbc    `# input file: out.raw` \
     $arg2           `# output to $arg2`
-  rm out-$hash.raw
+  rm out-$hash.sbc
 }
 
 wav_to_txt() {
@@ -22,17 +22,17 @@ wav_to_txt() {
     -ac 1              `# audio channel: #1` \
     -aq 16             `# audio quality: 16 (for SBC this means bitpool=16)` \
     -map_metadata -1   `# ????` \
-    out-$hash.raw            `# output to out.raw`
+    out-$hash.sbc            `# output to out.raw`
 
-  sbcinfo out-$hash.raw
-  xxd -i out-$hash.raw            `# output in C include file style` \
+  # sbcinfo out-$hash.sbc
+  xxd -i out-$hash.sbc            `# output in C include file style` \
     | head -n -2              `# skip last two xxd outline lines (skip C formatting)` \
     | tail -n +2              `# start output on line 2 of xxd output (skip C formatting)` \
     | sed 's/ //g'            `# remove spaces` \
     | tr --delete '\n'        `# remove newlines` \
     | sed 's/,/\,\n/16; P; D' `# collect into lines with the right length` \
     > $arg2
-  rm out-$hash.raw
+  rm out-$hash.sbc
 }
 
 [ "${1}" = "-T" ] || [ "${1}" = "--txt-to-wav" ] && shift 1 && args=$@ && arg1=$(echo $args | cut -d" " -f1) && arg2=$(echo $args | cut -d" " -f2) && txt_to_wav && exit
