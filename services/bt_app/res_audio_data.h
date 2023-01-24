112175c3fe8168860908c5fcd1b2b8fea0493231
Thatcher Chamberlin <j.thatcher.c@gmail.com>
2023-01-23 20:36:18 -0500

    Changed used variable names to match generated variable names

--- services/bt_app/res_audio_data.h
+++ services/bt_app/res_audio_data.h
@@ -16,9 +16,9 @@
 
 #ifdef MEDIA_PLAYER_SUPPORT
 
-extern const uint8_t EN_POWER_ON [];
+extern const uint8_t EN_SOUND_POWER_ON [];
 
-extern const uint8_t EN_POWER_OFF [];
+extern const uint8_t EN_SOUND_POWER_OFF [];
 
 extern const uint8_t EN_SOUND_ZERO[];
 
@@ -40,57 +40,57 @@ extern const uint8_t EN_SOUND_EIGHT [];
 
 extern const uint8_t EN_SOUND_NINE [];
 
-extern const uint8_t EN_BT_PAIR_ENABLE[];
+extern const uint8_t EN_SOUND_PAIR_ENABLE[];
 
-extern const uint8_t EN_BT_PAIRING[];
+extern const uint8_t EN_SOUND_PAIRING[];
 
-extern const uint8_t EN_BT_PAIRING_FAIL[];
+extern const uint8_t EN_SOUND_PAIRING_FAIL[];
 
-extern const uint8_t EN_BT_PAIRING_SUCCESS[];
+extern const uint8_t EN_SOUND_PAIRING_SUCCESS[];
 
-extern const uint8_t EN_BT_REFUSE[];
+extern const uint8_t EN_SOUND_REFUSE[];
 
-extern const uint8_t EN_BT_OVER[];
+extern const uint8_t EN_SOUND_OVER[];
 
-extern const uint8_t EN_BT_ANSWER[];
+extern const uint8_t EN_SOUND_ANSWER[];
 
-extern const uint8_t EN_BT_HUNG_UP[];
+extern const uint8_t EN_SOUND_HUNG_UP[];
 
-extern const uint8_t EN_BT_CONNECTED [];
+extern const uint8_t EN_SOUND_CONNECTED [];
 
-extern const uint8_t EN_BT_DIS_CONNECT [];
+extern const uint8_t EN_SOUND_DIS_CONNECT [];
 
-extern const uint8_t EN_BT_INCOMING_CALL[];
+extern const uint8_t EN_SOUND_INCOMING_CALL[];
 
-extern const uint8_t EN_CHARGE_PLEASE[];
+extern const uint8_t EN_SOUND_CHARGE_PLEASE[];
 
-extern const uint8_t EN_CHARGE_FINISH[];
+extern const uint8_t EN_SOUND_CHARGE_FINISH[];
 
-extern const uint8_t EN_LANGUAGE_SWITCH[];
+extern const uint8_t EN_SOUND_LANGUAGE_SWITCH[];
 
-extern const uint8_t EN_BT_WARNING[];
+extern const uint8_t EN_SOUND_WARNING[];
 
-extern const uint8_t EN_BT_ALEXA_START[];
+extern const uint8_t EN_SOUND_ALEXA_START[];
 
-extern const uint8_t EN_BT_ALEXA_STOP[];
+extern const uint8_t EN_SOUND_ALEXA_STOP[];
 
-extern const uint8_t EN_BT_GSOUND_MIC_OPEN[];
+extern const uint8_t EN_SOUND_GSOUND_MIC_OPEN[];
 
-extern const uint8_t EN_BT_GSOUND_MIC_CLOSE[];
+extern const uint8_t EN_SOUND_GSOUND_MIC_CLOSE[];
 
-extern const uint8_t EN_BT_DUDU[];
+extern const uint8_t EN_DUDU[];
 
-extern const uint8_t EN_BT_GSOUND_NC[];
-
-
+extern const uint8_t EN_SOUND_GSOUND_NC[];
 
 
 
 
 
-extern unsigned int EN_POWER_ON_len;
 
-extern unsigned int EN_POWER_OFF_len;
+
+extern unsigned int EN_SOUND_POWER_ON_len;
+
+extern unsigned int EN_SOUND_POWER_OFF_len;
 
 extern unsigned int EN_SOUND_ZERO_len;
 
@@ -112,53 +112,53 @@ extern unsigned int EN_SOUND_EIGHT_len;
 
 extern unsigned int EN_SOUND_NINE_len;
 
-extern unsigned int EN_BT_PAIR_ENABLE_len;
+extern unsigned int EN_SOUND_PAIR_ENABLE_len;
 
-extern unsigned int EN_BT_PAIRING_len;
+extern unsigned int EN_SOUND_PAIRING_len;
 
-extern unsigned int EN_BT_PAIRING_FAIL_len;
+extern unsigned int EN_SOUND_PAIRING_FAIL_len;
 
-extern unsigned int EN_BT_PAIRING_SUCCESS_len;
+extern unsigned int EN_SOUND_PAIRING_SUCCESS_len;
 
-extern unsigned int EN_BT_REFUSE_len;
+extern unsigned int EN_SOUND_REFUSE_len;
 
-extern unsigned int EN_BT_OVER_len;
+extern unsigned int EN_SOUND_OVER_len;
 
-extern unsigned int EN_BT_ANSWER_len;
+extern unsigned int EN_SOUND_ANSWER_len;
 
-extern unsigned int EN_BT_HUNG_UP_len;
+extern unsigned int EN_SOUND_HUNG_UP_len;
 
-extern unsigned int EN_BT_CONNECTED_len;
+extern unsigned int EN_SOUND_CONNECTED_len;
 
-extern unsigned int EN_BT_DIS_CONNECT_len;
+extern unsigned int EN_SOUND_DIS_CONNECT_len;
 
-extern unsigned int EN_BT_INCOMING_CALL_len;
+extern unsigned int EN_SOUND_INCOMING_CALL_len;
 
-extern unsigned int EN_CHARGE_PLEASE_len;
+extern unsigned int EN_SOUND_CHARGE_PLEASE_len;
 
-extern unsigned int EN_CHARGE_FINISH_len;
+extern unsigned int EN_SOUND_CHARGE_FINISH_len;
 
-extern unsigned int EN_LANGUAGE_SWITCH_len;
+extern unsigned int EN_SOUND_LANGUAGE_SWITCH_len;
 
-extern unsigned int EN_BT_WARNING_len;
+extern unsigned int EN_SOUND_WARNING_len;
 
-extern unsigned int EN_BT_ALEXA_START_len;
+extern unsigned int EN_SOUND_ALEXA_START_len;
 
-extern unsigned int EN_BT_ALEXA_STOP_len;
+extern unsigned int EN_SOUND_ALEXA_STOP_len;
 
-extern unsigned int EN_BT_GSOUND_MIC_OPEN_len;
+extern unsigned int EN_SOUND_GSOUND_MIC_OPEN_len;
 
-extern unsigned int EN_BT_GSOUND_MIC_CLOSE_len;
+extern unsigned int EN_SOUND_GSOUND_MIC_CLOSE_len;
 
-extern unsigned int EN_BT_DUDU_len;
+extern unsigned int EN_DUDU_len;
 
-extern unsigned int EN_BT_GSOUND_NC_len;
+extern unsigned int EN_SOUND_GSOUND_NC_len;
 
 
 
 #ifdef __INTERACTION__
-extern const uint8_t EN_BT_FINDME[];
-extern unsigned int EN_BT_FINDME_len;
+extern const uint8_t EN_SOUND_FINDME[];
+extern unsigned int EN_SOUND_FINDME_len;
 #endif
 
 #endif
