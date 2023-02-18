32aececcaf1e67ea96d2ac8eb989f40c6c490efb
Finlay Davidson <finlay.davidson@coderclass.nl>
2023-02-18 12:51:03 +0100

    Switch to opus by default, and support any format

--- convert.sh
+++ convert.sh
@@ -1,74 +1,79 @@
 #!/bin/sh
+
 txt_to_wav() {
-  hash=$(echo $arg1 | md5sum | cut -d" " -f1)
-  xxd -r -p $arg1 > out-$hash.sbc
-  # sbcinfo out-$hash.sbc
+  hash="$(md5sum "$1" | cut -d ' ' -f 1)"
+  filename="out-$hash.sbc"
+
+  xxd -r -p "$1" > "$filename"
+
   ffmpeg -y \
-    -v quiet         `# verbosity - other options are "quiet", "error", "panic"` \
-    -f sbc          `# accept SBC format` \
-    -ac 1           `# audio channel: #1` \
-    -i ./out-$hash.sbc    `# input file: out.raw` \
-    $arg2           `# output to $arg2`
-  rm out-$hash.sbc
+    -v quiet       `# verbosity - other options are "quiet", "error", "panic"` \
+    -f sbc         `# accept SBC format` \
+    -ac 1          `# audio channel: #1` \
+    -i "$filename" `# input file: out-$hash.sbc` \
+    "$2"           `# output to $2`
+
+  rm "$filename"
 }
 
 audio_to_txt() {
-  hash=$(echo $arg1 | md5sum | cut -d" " -f1)
-  ffmpeg -y \
-    -v quiet            `# verbosity - other options are "quiet", "error", "panic"` \
-    -i $arg1           `#input file: $arg1` \
-    -f sbc             `#output format: SBC` \
-    -ar 16000          `# audio rate: 16 kHz` \
-    -ac 1              `# audio channel: #1` \
-    -aq 16             `# audio quality: 16 (for SBC this means bitpool=16)` \
-    -map_metadata -1   `# ????` \
-    out-$hash.sbc            `# output to out.raw`
+  hash="$(md5sum "$1" | cut -d ' ' -f 1)"
+  filename="out-$hash.sbc"
 
-  # sbcinfo out-$hash.sbc
-  xxd -i out-$hash.sbc            `# output in C include file style` \
+  ffmpeg -y \
+    -v quiet                  `# verbosity - other options are "quiet", "error", "panic"` \
+    -i "$1"                   `# input file: $1` \
+    -f sbc                    `# output format: SBC` \
+    -ar 16000                 `# audio rate: 16 kHz` \
+    -ac 1                     `# audio channel: #1` \
+    -aq 16                    `# audio quality: 16 (for SBC this means bitpool=16)` \
+    -map_metadata -1          `# strip metadata` \
+    "$filename"               `# output to out-$hash.sbc`
+
+  xxd -i "$filename"          `# output in C include file style` \
     | head -n -2              `# skip last two xxd outline lines (skip C formatting)` \
     | tail -n +2              `# start output on line 2 of xxd output (skip C formatting)` \
     | sed 's/ //g'            `# remove spaces` \
     | tr --delete '\n'        `# remove newlines` \
     | sed 's/,/\,\n/16; P; D' `# collect into lines with the right length` \
-    > $arg2
-  rm out-$hash.sbc
+    > "$2"
+
+  rm "$filename"
 }
 
 audio_to_cpp() {
-  hash=$(echo $arg1 | md5sum | cut -d" " -f1)
-  filename="out-$hash.sbc"
-  varname=$(echo $arg1 | rev | cut -c5- | cut -d"/" -f1 | rev | tr '[:lower:]' '[:upper:]' | tr "/" _)
+  varname="$(basename "$1" | rev | cut -f 2- -d '.' | rev | tr '[:lower:]/' '[:upper:]_')"
+
   ffmpeg -y \
-    -v quiet            `# verbosity - other options are "quiet", "error", "panic"` \
-    -i $arg1           `#input file: $arg1` \
-    -f sbc             `#output format: SBC` \
-    -ar 16000          `# audio rate: 16 kHz` \
-    -ac 1              `# audio channel: #1` \
-    -aq 16             `# audio quality: 16 (for SBC this means bitpool=16)` \
-    -map_metadata -1   `# ????` \
-    $varname            `# output to something like EN_SOUND_POWER_ON`
+    -v quiet         `# verbosity - other options are "quiet", "error", "panic"` \
+    -i "$1"          `# input file: $1` \
+    -f sbc           `# output format: SBC` \
+    -ar 16000        `# audio rate: 16 kHz` \
+    -ac 1            `# audio channel: #1` \
+    -aq 16           `# audio quality: 16 (for SBC this means bitpool=16)` \
+    -map_metadata -1 `# strip metadata` \
+    "$varname"       `# output to something like EN_SOUND_POWER_ON`
 
-  echo "#include <stdint.h>\n" > $arg2
+  printf '#include <stdint.h>\n' > "$2"
 
-  # sbcinfo out-$hash.sbc
-  xxd -i $varname            `# output in C include file style` \
-    >> $arg2
+  xxd -i "$varname"  `# output in C include file style` \
+    >> "$2"
 
-  sed -i "s/unsigned char/uint8_t/g" $arg2
-  
-  #rm $filename
+  sed -i 's/unsigned char/uint8_t/g' "$2"
+
+  rm "$varname"
 }
 
-[ "${1}" = "-T" ] || [ "${1}" = "--txt-to-wav"   ] && shift 1 && args=$@ && arg1=$(echo $args | cut -d" " -f1) && arg2=$(echo $args | cut -d" " -f2) && txt_to_wav && exit
-[ "${1}" = "-A" ] || [ "${1}" = "--audio-to-txt" ] && shift 1 && args=$@ && arg1=$(echo $args | cut -d" " -f1) && arg2=$(echo $args | cut -d" " -f2) && audio_to_txt && exit
-[ "${1}" = "-C" ] || [ "${1}" = "--audio-to-cpp" ] && shift 1 && args=$@ && arg1=$(echo $args | cut -d" " -f1) && arg2=$(echo $args | cut -d" " -f2) && audio_to_cpp && exit
+[ "$1" = '-T' ] || [ "$1" = '--txt-to-wav'   ] && shift 1 && txt_to_wav "$@" && exit
+[ "$1" = '-A' ] || [ "$1" = '--audio-to-txt' ] && shift 1 && audio_to_txt "$@" && exit
+[ "$1" = '-C' ] || [ "$1" = '--audio-to-cpp' ] && shift 1 && audio_to_cpp "$@" && exit
+
 echo "
 Sound format converter:
 Usage:
-  ./convert.sh [option] [input-file] [output-file]
+  $0 [option] [input-file] [output-file]
 Options:
--T or --txt-to-wav   Converts the text file to a wav audio file
--A or --audio-to-txt Converts a wav file to a file readable by the old pinebuds firmware
--C or --audio-to-cpp Converts a wav file to a file compilable into the pinebuds firmware
+  -T or --txt-to-wav   Converts the text file to a wav audio file
+  -A or --audio-to-txt Converts an audio file to a file readable by the old pinebuds firmware
+  -C or --audio-to-cpp Converts an audio file to a file compilable into the pinebuds firmware
 "
--- services/bt_app/res_audio_data.h
+++ services/bt_app/res_audio_data.h
@@ -1,3 +1,5 @@
+#pragma once
+
 /***************************************************************************
  *
  * Copyright 2015-2019 BES.
@@ -13,148 +15,75 @@
  * trademark and other intellectual property rights.
  *
  ****************************************************************************/
-
 #ifdef MEDIA_PLAYER_SUPPORT
 
-extern const uint8_t SOUND_POWER_ON [];
-
-extern const uint8_t SOUND_POWER_OFF [];
-
+extern const uint8_t SOUND_POWER_ON[];
+extern const uint8_t SOUND_POWER_OFF[];
 extern const uint8_t SOUND_ZERO[];
-
 extern const uint8_t SOUND_ONE[];
-
 extern const uint8_t SOUND_TWO[];
-
 extern const uint8_t SOUND_THREE[];
-
 extern const uint8_t SOUND_FOUR[];
-
 extern const uint8_t SOUND_FIVE[];
-
 extern const uint8_t SOUND_SIX[];
-
-extern const uint8_t SOUND_SEVEN [];
-
-extern const uint8_t SOUND_EIGHT [];
-
-extern const uint8_t SOUND_NINE [];
-
+extern const uint8_t SOUND_SEVEN[];
+extern const uint8_t SOUND_EIGHT[];
+extern const uint8_t SOUND_NINE[];
 extern const uint8_t SOUND_PAIR_ENABLE[];
-
 extern const uint8_t SOUND_PAIRING[];
-
 extern const uint8_t SOUND_PAIRING_FAIL[];
-
 extern const uint8_t SOUND_PAIRING_SUCCESS[];
-
 extern const uint8_t SOUND_REFUSE[];
-
 extern const uint8_t SOUND_OVER[];
-
 extern const uint8_t SOUND_ANSWER[];
-
 extern const uint8_t SOUND_HUNG_UP[];
-
-extern const uint8_t SOUND_CONNECTED [];
-
-extern const uint8_t SOUND_DIS_CONNECT [];
-
+extern const uint8_t SOUND_CONNECTED[];
+extern const uint8_t SOUND_DIS_CONNECT[];
 extern const uint8_t SOUND_INCOMING_CALL[];
-
 extern const uint8_t SOUND_CHARGE_PLEASE[];
-
 extern const uint8_t SOUND_CHARGE_FINISH[];
-
 extern const uint8_t SOUND_LANGUAGE_SWITCH[];
-
 extern const uint8_t SOUND_WARNING[];
-
 extern const uint8_t SOUND_ALEXA_START[];
-
 extern const uint8_t SOUND_ALEXA_STOP[];
-
 extern const uint8_t SOUND_GSOUND_MIC_OPEN[];
-
 extern const uint8_t SOUND_GSOUND_MIC_CLOSE[];
-
 extern const uint8_t DUDU[];
-
 extern const uint8_t SOUND_GSOUND_NC[];
 
-
-
-
-
-
-
-extern unsigned int SOUND_POWER_ON_len;
-
-extern unsigned int SOUND_POWER_OFF_len;
-
-extern unsigned int SOUND_ZERO_len;
-
-extern unsigned int SOUND_ONE_len;
-
-extern unsigned int SOUND_TWO_len;
-
-extern unsigned int SOUND_THREE_len;
-
-extern unsigned int SOUND_FOUR_len;
-
-extern unsigned int SOUND_FIVE_len;
-
-extern unsigned int SOUND_SIX_len;
-
-extern unsigned int SOUND_SEVEN_len;
-
-extern unsigned int SOUND_EIGHT_len;
-
-extern unsigned int SOUND_NINE_len;
-
-extern unsigned int SOUND_PAIR_ENABLE_len;
-
-extern unsigned int SOUND_PAIRING_len;
-
-extern unsigned int SOUND_PAIRING_FAIL_len;
-
-extern unsigned int SOUND_PAIRING_SUCCESS_len;
-
-extern unsigned int SOUND_REFUSE_len;
-
-extern unsigned int SOUND_OVER_len;
-
-extern unsigned int SOUND_ANSWER_len;
-
-extern unsigned int SOUND_HUNG_UP_len;
-
-extern unsigned int SOUND_CONNECTED_len;
-
-extern unsigned int SOUND_DIS_CONNECT_len;
-
-extern unsigned int SOUND_INCOMING_CALL_len;
-
-extern unsigned int SOUND_CHARGE_PLEASE_len;
-
-extern unsigned int SOUND_CHARGE_FINISH_len;
-
-extern unsigned int SOUND_LANGUAGE_SWITCH_len;
-
-extern unsigned int SOUND_WARNING_len;
-
-extern unsigned int SOUND_ALEXA_START_len;
-
-extern unsigned int SOUND_ALEXA_STOP_len;
-
-extern unsigned int SOUND_GSOUND_MIC_OPEN_len;
-
-extern unsigned int SOUND_GSOUND_MIC_CLOSE_len;
-
-extern unsigned int DUDU_len;
-
-extern unsigned int SOUND_GSOUND_NC_len;
-
-
+extern const unsigned SOUND_POWER_ON_len;
+extern const unsigned SOUND_POWER_OFF_len;
+extern const unsigned SOUND_ZERO_len;
+extern const unsigned SOUND_ONE_len;
+extern const unsigned SOUND_TWO_len;
+extern const unsigned SOUND_THREE_len;
+extern const unsigned SOUND_FOUR_len;
+extern const unsigned SOUND_FIVE_len;
+extern const unsigned SOUND_SIX_len;
+extern const unsigned SOUND_SEVEN_len;
+extern const unsigned SOUND_EIGHT_len;
+extern const unsigned SOUND_NINE_len;
+extern const unsigned SOUND_PAIR_ENABLE_len;
+extern const unsigned SOUND_PAIRING_len;
+extern const unsigned SOUND_PAIRING_FAIL_len;
+extern const unsigned SOUND_PAIRING_SUCCESS_len;
+extern const unsigned SOUND_REFUSE_len;
+extern const unsigned SOUND_OVER_len;
+extern const unsigned SOUND_ANSWER_len;
+extern const unsigned SOUND_HUNG_UP_len;
+extern const unsigned SOUND_CONNECTED_len;
+extern const unsigned SOUND_DIS_CONNECT_len;
+extern const unsigned SOUND_INCOMING_CALL_len;
+extern const unsigned SOUND_CHARGE_PLEASE_len;
+extern const unsigned SOUND_CHARGE_FINISH_len;
+extern const unsigned SOUND_LANGUAGE_SWITCH_len;
+extern const unsigned SOUND_WARNING_len;
+extern const unsigned SOUND_ALEXA_START_len;
+extern const unsigned SOUND_ALEXA_STOP_len;
+extern const unsigned SOUND_GSOUND_MIC_OPEN_len;
+extern const unsigned SOUND_GSOUND_MIC_CLOSE_len;
+extern const unsigned DUDU_len;
+extern const unsigned SOUND_GSOUND_NC_len;
 
 #ifdef __INTERACTION__
 extern const uint8_t SOUND_FINDME[];
