6d95360c51d39bee6a20ebb28ec0d1988790b614
Thatcher Chamberlin <j.thatcher.c@gmail.com>
2023-02-18 08:35:32 -0500

    Add language configuration to build.sh!

--- build.sh
+++ build.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
-make -j T=open_source DEBUG=1 >log.txt 2>&1
+
+# use LANG environment variable, default to "en"
+LANG="${LANG:-en}"
+
+make -j T=open_source LANG=${LANG} DEBUG=1 >log.txt 2>&1
 
 if [ $? -eq 0 ]; then
 	echo "build success"
