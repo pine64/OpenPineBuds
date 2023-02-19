e9ddfd6c4a82cbba35695e3efe665a97541446b2
Thatcher Chamberlin <j.thatcher.c@gmail.com>
2023-02-19 14:28:49 -0500

    Update build.sh
    Co-authored-by: FintasticMan <52415484+FintasticMan@users.noreply.github.com>

--- build.sh
+++ build.sh
@@ -3,7 +3,7 @@
 # use LANG environment variable, default to "en"
 LANG="${LANG:-en}"
 
-make -j T=open_source LANG=${LANG} DEBUG=1 >log.txt 2>&1
+make -j T=open_source DEBUG=1 >log.txt 2>&1
 
 if [ $? -eq 0 ]; then
 	echo "build success"
