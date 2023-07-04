c76a31e13a9f3f4b1c2c8f1e21b77638dca3b501
Ben V. Brown <Ralim@Ralimtek.com>
2023-07-04 23:13:52 +1000

    Update decoder

--- dev_tools/anc_decoder/src/main.rs
+++ dev_tools/anc_decoder/src/main.rs
@@ -71,22 +71,27 @@ struct aud_item {
 
 #[derive(Debug, PartialEq, Encode, Decode, Copy, Clone, Default)]
 struct struct_anc_cfg {
-    //
+    //V1+
     anc_cfg_ff_l: aud_item,
     anc_cfg_ff_r: aud_item,
     anc_cfg_fb_l: aud_item,
     anc_cfg_fb_r: aud_item,
+    //V2
+    anc_cfg_tt_l: aud_item,
+    anc_cfg_tt_r: aud_item,
+    anc_cfg_mc_l: aud_item,
+    anc_cfg_mc_r: aud_item,
 }
 
 #[derive(Debug, PartialEq, Encode, Decode, Copy, Clone, Default)]
 struct pctool_struct_anc_cfg {
     //
-    anc_cfg: [struct_anc_cfg; 2],
+    anc_cfg: [struct_anc_cfg; 2], //0 == 41k, 2==44k
 }
 
 #[derive(Debug, PartialEq, Encode, Decode, Copy, Clone, Default)]
 struct AudSectionBodyConfig {
-    anc_config_arr: [pctool_struct_anc_cfg; 4],
+    anc_config_arr: [pctool_struct_anc_cfg; 1], // we may only use first of 4 entries
 }
 #[derive(Debug, PartialEq, Encode, Decode, Copy, Clone, Default)]
 struct AudSectionBody {
